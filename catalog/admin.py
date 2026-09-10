50849e014d12d827acde395aaa6fbaacb82046c9
Nathan Kirby <natekirbz@gmail.com>
2026-09-09 22:42:16 -0400

    book challenge

--- catalog/admin.py
+++ catalog/admin.py
@@ -15,13 +15,19 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+
 # Define the admin class
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
+    inlines = [BooksInline]
+
 
 # Register the Admin classes for Book using the decorator
 class BooksInstanceInline(admin.TabularInline):
@@ -32,3 +38,18 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models."""
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
